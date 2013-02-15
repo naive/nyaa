98aa6d32e5419011dec57c377ecc5123d76825eb
David Palma <requiem.der.seele@gmail.com>
2013-02-15 01:06:44 -0600

    Add info attribute to torrent
    o Grab description link so we can forward user to info page

--- lib/nyaa/torrent.rb
+++ lib/nyaa/torrent.rb
@@ -2,12 +2,13 @@
 module Nyaa
 
   class Torrent
-    attr_accessor :name, :link, :filesize, :seeders, :leechers
+    attr_accessor :name, :info, :link, :filesize, :seeders, :leechers
     attr_accessor :category, :status, :downloads, :comments
     attr_accessor :health, :bytes
 
     def initialize (row = nil)
       self.name     = row.css('td.tlistname').at('a').text.strip
+      self.info     = row.css('td.tlistname').at('a')['href']
       self.link     = row.css('td.tlistdownload').at('a')['href']
       self.filesize = row.css('td.tlistsize').text
       self.seeders  = row.css('td.tlistsn').text.to_i
