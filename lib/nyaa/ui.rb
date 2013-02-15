ed9c780f8257e7f9c6f257d96addbfe3c6cdaba1
David Palma <requiem.der.seele@gmail.com>
2013-02-15 16:20:21 -0600

    Major curses UI updates
    o Implement status bar
    o Implment open for view items in browser
    o Implement get for downloading items
    o Implment menu for displaying subset of items
    o Implment menu movement
    o Tweaks to header

--- lib/nyaa/ui.rb
+++ lib/nyaa/ui.rb
@@ -2,8 +2,11 @@ include Curses
 
 module Nyaa
   class UI
-    def initialize (config)
+    attr_accessor :menusize
+
+    def initialize (config, search)
       @config = config
+      @status = { :text => '', :type => :default }
       setup_curses
 
       # columns
@@ -20,6 +23,12 @@ module Nyaa
         'p' => 'prev',
         'q' => 'quit',
       }
+
+      # fetch single batch - testing
+      @torrents = search.more.get_results
+      @num_torrents = @torrents.size
+      
+      @menusize = lines - 3 # lines - header + footer + status
     end
 
     def header
@@ -31,9 +40,10 @@ module Nyaa
       @name_column_width = cols - (@info_columns.length * @info_column_width)
 
       cat = CATS[@config[:category].to_sym][:title]
+      header_text = sprintf " %-#{@name_column_width-1}s%s", "NyaaBETA - #{cat}", @info_columns.join
       attrset(color_pair(1))
       setpos(0,0)
-      addstr(sprintf " %-#{@name_column_width-1}s%s", "Nyaa - #{cat}", @info_columns.join)
+      addstr(sprintf "%-#{cols}s", header_text)
     end
 
     def footer
@@ -43,60 +53,92 @@ module Nyaa
       addstr(sprintf " %-#{cols}s", footer_text)
     end
 
-    def content
-      rows = lines - 2 # lines - header + footer
+    def status(text = nil, type = nil)
+      @status[:text] = text if text
+      @status[:type] = type if type
+
+      case @status[:type]
+      when :success then profile = 8
+      when :failure then profile = 9
+      else profile = 2
+      end
+
+      status_text = sprintf "%-28s", @status[:text]
+      attrset(color_pair(profile))
+      setpos(lines-2,0)
+      addstr(sprintf " %-#{cols}s", status_text)
+    end
+
+    def menu(highlight)
       xpos = 1
       attrset(color_pair(0))
       setpos(xpos, 0)
 
-      nyaa = Nyaa::Search.new(@config[:query], @config[:category], @config[:filter])
-      @results = nyaa.more.get_results
-      @results.each do |r|
-        line = sprintf "% -#{@name_column_width}s %9s %9.d %9.d", r.name, r.filesize, r.seeders, r.leechers
-        attrset(color_pair(torrent_status(r)))
-        setpos(xpos, 0)
-        addstr(line)
-        xpos += 1
+      (0..@menusize-1).each do |i|
+
+        if i < @num_torrents
+          line_text = sprintf("% -#{@name_column_width}s %9s %9s %9s",
+            truncate(@torrents[i].name, @name_column_width),
+            @torrents[i].filesize,
+            @torrents[i].seeders,
+            @torrents[i].leechers)
+
+          attrset(color_pair(torrent_status(@torrents[i])))
+          setpos(xpos, 0)
+          # highlight the present choice
+          if highlight == i + 1
+            attron(A_STANDOUT) # Enables the best highlighting mode of the terminal
+            addstr(line_text)
+            attroff(A_STANDOUT)
+          else
+            addstr(line_text)
+          end
+          xpos += 1
+        else
+          # blank lines if there's < @menusize of results
+          line_text = " "*cols
+        end 
+
+      end # lines
+    end # menu
+
+    def select
+    end
+
+    def help
+    end
+
+    def get(choice)
+      torrent = @torrents[choice-1]
+      download = Downloader.new(torrent.link, @config[:output])
+      download.save
+      unless download.failed?
+        status("Downloaded '#{download.filename}' successfully.", :success)
+      else
+        status('Download failed (3 attempts)', :failure)
       end
     end
 
-    def move(line, column)
-      attrset(color_pair(5)) 
-      setpos(line, column)
-    end
-
-    def torrent_status(torrent)
-      case torrent.status
-      when 'A+'      then 8
-      when 'Trusted' then 6
-      when 'Remake'  then 10
-      else 4
-      end
-    end
-
-    def cmd_help
-
-    end
-
-    def cmd_get
-    end
-
-    def cmd_info(torrent)
-      link = torrent.info
+    def open(choice)
+      torrent = @torrents[choice-1]
+      link = "#{torrent.info}"
       if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/ then
           system("start #{link}")
       elsif RbConfig::CONFIG['host_os'] =~ /darwin/ then
-          system("open #{link}")
+          system("open '#{link}'", [:out, :err]=>'/dev/null')
       elsif RbConfig::CONFIG['host_os'] =~ /linux/ then
-          system("xdg-open #{link}")
+          system("xdg-open '#{link}'", [:out, :err]=>'/dev/null')
       end
+      status("Loading #{link}...", :success)
     end
 
-    def cmd_next
+    def paginate
     end
 
-    def cmd_prev
+    def reverse_paginate
     end
+
+    private 
 
     def setup_curses
       noecho # don't show typed keys
@@ -111,14 +153,31 @@ module Nyaa
         init_pair(1, COLOR_BLACK, COLOR_CYAN)    # ui:header
         init_pair(2, COLOR_BLACK, COLOR_CYAN)    # ui:footer
         init_pair(3, COLOR_MAGENTA, COLOR_BLACK) # ui:selected
-        init_pair(4, COLOR_WHITE, COLOR_BLACK)   # status:normal
-        init_pair(5, COLOR_BLACK, COLOR_WHITE)   # status:normal hover
-        init_pair(6, COLOR_GREEN, COLOR_BLACK)   # status:trusted
-        init_pair(7, COLOR_BLACK, COLOR_GREEN)   # status:trusted hover
-        init_pair(8, COLOR_BLUE, COLOR_BLACK)    # status:aplus
-        init_pair(9, COLOR_BLACK, COLOR_BLUE)    # status:aplus hover
-        init_pair(10, COLOR_RED, COLOR_BLACK)    # status:remake
-        init_pair(11, COLOR_BLACK, COLOR_RED)    # status:remake hover
+
+        init_pair(4, COLOR_WHITE, COLOR_BLACK)   # torrent:normal
+        init_pair(5, COLOR_GREEN, COLOR_BLACK)   # torrent:trusted
+        init_pair(6, COLOR_BLUE, COLOR_BLACK)    # torrent:aplus
+        init_pair(7, COLOR_RED, COLOR_BLACK)    # torrent:remake
+
+        init_pair(8, COLOR_BLACK, COLOR_GREEN)  # type:success
+        init_pair(9, COLOR_BLACK, COLOR_RED)    # type:failure
+      end
+    end
+
+    def torrent_status(torrent)
+      case torrent.status
+      when 'Trusted' then 5
+      when 'A+'      then 6
+      when 'Remake'  then 7
+      else 4
+      end
+    end
+
+    def truncate(text, width)
+      if text.length > width
+        truncated = "#{text[0..width-4]}..."
+      else
+        text
       end
     end
 
