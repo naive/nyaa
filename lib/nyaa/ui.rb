afcaadd10182046b00b2ca518b524e52ad2ba247
David Palma <requiem.der.seele@gmail.com>
2013-02-17 00:48:05 -0600

    Add pagination, various fixes to the UI
    o Implement pagination (next, prev) in UI
    o Add menusize and page accessors for UI
    o Add background harvester for fetching results in a sep. thread
    o Add search summary bar
    o Offset fixes to get(), info(), and menu()
    o Some minor tweaks to the curses config

--- lib/nyaa/ui.rb
+++ lib/nyaa/ui.rb
@@ -2,7 +2,7 @@ include Curses
 
 module Nyaa
   class UI
-    attr_accessor :menusize
+    attr_accessor :menusize, :page
 
     def initialize (config, search)
       @config = config
@@ -24,11 +24,16 @@ module Nyaa
         'q' => 'quit',
       }
 
-      # fetch single batch - testing
-      @torrents = search.more.get_results
+      @search = search
+      @torrents = @search.more.results
       @num_torrents = @torrents.size
+      harvester # start bg harvester
       
-      @menusize = lines - 3 # lines - header + footer + status
+      @menusize = lines - 4 # lines - header + footer + status
+      @page = 1
+      @num_pages = (@search.count/@menusize.to_f).ceil
+      @offset = 0
+      # TODO: on hold state: prevent paging forward when waiting on results
     end
 
     def header
@@ -39,8 +44,9 @@ module Nyaa
       # key column width is whatever is leftover
       @name_column_width = cols - (@info_columns.length * @info_column_width)
 
+      # header bar
       cat = CATS[@config[:category].to_sym][:title]
-      header_text = sprintf " %-#{@name_column_width-1}s%s", "NyaaBETA - #{cat}", @info_columns.join
+      header_text = sprintf " %-#{@name_column_width-1}s%s", "Nyaa - #{cat}", @info_columns.join
       attrset(color_pair(1))
       setpos(0,0)
       addstr(sprintf "%-#{cols}s", header_text)
@@ -51,22 +57,30 @@ module Nyaa
       attrset(color_pair(2))
       setpos(lines - 1, 0)
       addstr(sprintf " %-#{cols}s", footer_text)
+
+      search_summary = sprintf " %-14s %-14s %-14s",
+        "view: [#{@offset+1}-#{@offset+@menusize}]/#{@search.count}",
+        "recv: #{@num_torrents}/#{@search.count}",
+        "page: #{@page}/#{@num_pages}"
+      attrset(color_pair(2))
+      setpos(lines - 2, 0)
+      addstr(sprintf "%-#{cols}s", search_summary)
     end
 
     def status(text = nil, type = nil)
       @status[:text] = text if text
-      @status[:type] = type if type
+      @status[:type] = type
 
       case @status[:type]
       when :success then profile = 8
       when :failure then profile = 9
-      else profile = 2
+      else profile = 1
       end
 
-      status_text = sprintf "%-28s", @status[:text]
+      status_text = sprintf " %-28s", @status[:text]
       attrset(color_pair(profile))
-      setpos(lines-2,0)
-      addstr(sprintf " %-#{cols}s", status_text)
+      setpos(lines-3,0)
+      addstr(sprintf "%-#{cols}s", status_text)
     end
 
     def menu(highlight)
@@ -75,52 +89,58 @@ module Nyaa
       setpos(xpos, 0)
 
       (0..@menusize-1).each do |i|
-
-        if i < @num_torrents
-          line_text = sprintf("% -#{@name_column_width}s %9s %9s %9s",
-            truncate(@torrents[i].name, @name_column_width),
-            @torrents[i].filesize,
-            @torrents[i].seeders,
-            @torrents[i].leechers)
-
-          attrset(color_pair(torrent_status(@torrents[i])))
-          setpos(xpos, 0)
-          # highlight the present choice
-          if highlight == i + 1
-            attron(A_STANDOUT) # Enables the best highlighting mode of the terminal
-            addstr(line_text)
-            attroff(A_STANDOUT)
+        if i < @search.count - @offset
+          if @torrents[@offset + i].nil?
+            status("Fetching more results, try again.", :failure)
           else
-            addstr(line_text)
+            line_text = sprintf("% -#{@name_column_width}s %9s %9s %9s",
+              truncate("#{@torrents[@offset + i].name}", @name_column_width),
+              @torrents[@offset + i].filesize,
+              @torrents[@offset + i].seeders,
+              @torrents[@offset + i].leechers)
+
+            attrset(color_pair(torrent_status(@torrents[@offset + i])))
+            setpos(xpos, 0)
+            # highlight the present choice
+            if highlight == i + 1
+              attron(A_STANDOUT)
+              addstr(line_text)
+              attroff(A_STANDOUT)
+            else
+              addstr(line_text)
+            end
+            xpos += 1
           end
-          xpos += 1
         else
           # blank lines if there's < @menusize of results
           line_text = " "*cols
+          addstr(line_text)
+          xpos += 1
         end 
+      end
+    end
 
-      end # lines
-    end # menu
-
+    # TODO: Multi select
     def select
     end
 
+      # TODO: Pop up help window
     def help
     end
 
     def get(choice)
-      torrent = @torrents[choice-1]
+      torrent = @torrents[@offset + choice - 1]
       download = Downloader.new(torrent.link, @config[:output])
       download.save
       unless download.failed?
-        status("Downloaded '#{download.filename}' successfully.", :success)
+        status("Downloaded successful.", :success)
       else
-        status('Download failed (3 attempts)', :failure)
+        status('Download failed (3 attempts).', :failure)
       end
     end
 
     def open(choice)
-      torrent = @torrents[choice-1]
+      torrent = @torrents[@offset + choice - 1]
       link = "#{torrent.info}"
       if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/ then
           system("start #{link}")
@@ -129,38 +149,72 @@ module Nyaa
       elsif RbConfig::CONFIG['host_os'] =~ /linux/ then
           system("xdg-open '#{link}'", [:out, :err]=>'/dev/null')
       end
-      status("Loading #{link}...", :success)
+      status("Opened '#{link}'", :success)
     end
 
-    def paginate
+    def next
+      unless @page + 1 > @num_pages
+        @page += 1
+      end
+
+      unless @offset + @menusize > @num_torrents
+        @offset += @menusize
+      end
     end
 
-    def reverse_paginate
+    def prev
+      unless @page - 1 < 1
+        @page += -1
+      end
+
+      unless @offset == 0
+        @offset -= @menusize
+      end
+    end
+
+    def resize_handler(cursor)
+      @menusize = lines - 4
+      menu(cursor)
+      refresh
+    end
+
+    def harvester
+      Thread.new do
+        until @num_torrents == @search.count
+          @torrents = @search.more.results
+          @num_torrents = @torrents.size
+          sleep 2
+        end
+        Thread.kill
+      end
     end
 
     private 
 
     def setup_curses
       noecho # don't show typed keys
-      init_screen
-      cbreak # make typed keys immediately acessible
-      curs_set(0)
+      init_screen # start curses mode
+      cbreak # disable line buffering
+      curs_set(0) # make cursor invisible
       stdscr.keypad(true) # enable arrow keys
+
+      # set keyboard input timeout - sneaky way to manage refresh rate
+      timeout = 500 # milliseconds
 
       if can_change_color?
         start_color
         init_pair(0, COLOR_WHITE, COLOR_BLACK)   # ui:default
         init_pair(1, COLOR_BLACK, COLOR_CYAN)    # ui:header
-        init_pair(2, COLOR_BLACK, COLOR_CYAN)    # ui:footer
+        init_pair(2, COLOR_BLACK, COLOR_YELLOW)  # ui:footer
         init_pair(3, COLOR_MAGENTA, COLOR_BLACK) # ui:selected
 
-        init_pair(4, COLOR_WHITE, COLOR_BLACK)   # torrent:normal
-        init_pair(5, COLOR_GREEN, COLOR_BLACK)   # torrent:trusted
-        init_pair(6, COLOR_BLUE, COLOR_BLACK)    # torrent:aplus
-        init_pair(7, COLOR_RED, COLOR_BLACK)    # torrent:remake
+        init_pair(4, COLOR_WHITE, COLOR_BLACK) # torrent:normal
+        init_pair(5, COLOR_GREEN, COLOR_BLACK) # torrent:trusted
+        init_pair(6, COLOR_BLUE, COLOR_BLACK)  # torrent:aplus
+        init_pair(7, COLOR_RED, COLOR_BLACK)   # torrent:remake
 
-        init_pair(8, COLOR_BLACK, COLOR_GREEN)  # type:success
-        init_pair(9, COLOR_BLACK, COLOR_RED)    # type:failure
+        init_pair(8, COLOR_BLACK, COLOR_GREEN) # type:success
+        init_pair(9, COLOR_BLACK, COLOR_RED)   # type:failure
       end
     end
 
